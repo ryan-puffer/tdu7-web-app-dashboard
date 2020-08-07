a21204eaf7f58ff0fc16d88ba790f76f27e4f205
ryan-puffer <ryanpufferdev@gmail.com>
2020-08-07 15:14:27 -0500

    add alert bar

---
+++ app.js
@@ -0,0 +1,15 @@
+const alertBanner = document.querySelector('#alert');
+
+alertBanner.innerHTML = `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">x</p>
+    </div>
+    `;
+
+alertBanner.addEventListener('click', (e) => {
+	const element = e.target;
+	if (element.classList.contains('alert-banner-close')) {
+		alertBanner.style.display = 'none';
+	}
+});
