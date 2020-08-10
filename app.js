5020da0279e85381fd57ac60d480815d1b0fd671
ryan-puffer <ryanpufferdev@gmail.com>
2020-08-10 10:43:33 -0500

    add new members and activity widget content

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const alertBanner = document.querySelector('#alert');
+const user = document.querySelector('#userField');
+const message = document.querySelector('#messageField');
+const submit = document.querySelector('#messageUserSubmit');
+
+//DISPLAY ALERT
 
 alertBanner.innerHTML = `
     <div class="alert-banner">
@@ -11,5 +16,19 @@ alertBanner.addEventListener('click', (e) => {
 	const element = e.target;
 	if (element.classList.contains('alert-banner-close')) {
 		alertBanner.style.display = 'none';
+	}
+});
+
+//MESSAGE USER
+
+submit.addEventListener('click', () => {
+	if (user.value === '' && message.value === '') {
+		alert('Please enter a user and message!');
+	} else if (user.value === '') {
+		alert('Please enter a user!');
+	} else if (message.value === '') {
+		alert('Please enter a message to the user!');
+	} else {
+		alert(`Message sent to ${user.value}!`);
 	}
 });
